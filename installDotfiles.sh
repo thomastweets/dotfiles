4f492e87ed6c324bc6f5f4732e95759d3e5265bd
Thomas Emmerling <git@thomasemmerling.de>
2014-05-03 16:24:33 +0200

    check for .emacs.d dir before linking

--- installDotfiles.sh
+++ installDotfiles.sh
@@ -4,5 +4,9 @@ ln -s ~/dotfiles/zshenv ~/.zshenv
 ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
 ln -s ~/dotfiles/gitconfig ~/.gitconfig
 ln -s ~/dotfiles/gitignore_global ~/.gitignore_global
+if [ ! -e ~/dotfiles/.emacs.d ]
+then
+	mkdir ~/.emacs.d
+fi
 ln -s ~/dotfiles/emacs.d/init.el ~/.emacs.d/init.el
 ln -s ~/dotfiles/emacs.d/modules ~/.emacs.d/modules
