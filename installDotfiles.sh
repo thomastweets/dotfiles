e4f801e12d95dc9945df37b4b907abfbbe9a420d
Thomas Emmerling <git@thomasemmerling.de>
2015-01-24 17:12:24 +0100

    Fix typo in installDotfiles.sh

--- installDotfiles.sh
+++ installDotfiles.sh
@@ -12,4 +12,4 @@ ln -s ~/dotfiles/atom/config.cson ~/.atom/config.cson
 ln -s ~/dotfiles/atom/keymap.cson ~/.atom/keymap.cson
 ln -s ~/dotfiles/atom/snippets.cson ~/.atom/snippets.cson
 ln -s ~/dotfiles/atom/init.coffee ~/.atom/init.coffee
-ln -s ~/dotfiles/atom/styles.less /.atom/styles.less
+ln -s ~/dotfiles/atom/styles.less ~/.atom/styles.less
