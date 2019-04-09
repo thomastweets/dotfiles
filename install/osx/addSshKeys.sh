200b2aacc6d5e41dbca375950cea5a766367fd5f
Thomas Emmerling <git@thomasemmerling.de>
2019-04-09 02:06:29 +0200

    Update 2019.

--- install/osx/addSshKeys.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/zsh
-
-ssh-add -K ~/.ssh/github_rsa
-ssh-add -K ~/.ssh/thomasemmerling_rsa
--- install/osx/installOtherSoftware.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-cd /Applications && curl http://www.ninjamonkeysoftware.com/slate/versions/slate-latest.tar.gz | tar -xz
